e796cfe72d7d99ed5c27b16657272c7fc13bad3c
Daniel Cruver <ddcruver@gmail.com>
2012-09-08 11:24:44 -0700

    Add dependency of part-0 to part-1

---
+++ part-1/src/main/java/edu/buffalo/cse/cse605/FDCoarse.java
@@ -0,0 +1,18 @@
+package edu.buffalo.cse.cse605;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ddcruver
+ * Date: 9/8/12
+ * Time: 11:18 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FDCoarse<T> extends FDList<T>
+{
+
+	public FDCoarse(T v)
+	{
+		super(v);
+	}
+
+}
