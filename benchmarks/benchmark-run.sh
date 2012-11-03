f307c50cf141ff03551f846ba24d0568b5c6c890
Dan <ddcruver@gmail.com>
2012-11-03 17:48:05 -0400

    Make benchmark script more flexible.

--- benchmarks/benchmark-run.sh
+++ benchmarks/benchmark-run.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-mvn exec:java -Dexec.mainClass="edu.buffalo.cse.cse605.BenchmarkRunner" -Dexec.args="1 10 20 10000" > 1-thread.out &
-mvn exec:java -Dexec.mainClass="edu.buffalo.cse.cse605.BenchmarkRunner" -Dexec.args="2 10 20 10000" > 2-thread.out &
-mvn exec:java -Dexec.mainClass="edu.buffalo.cse.cse605.BenchmarkRunner" -Dexec.args="4 10 20 10000" > 4-thread.out &
-mvn exec:java -Dexec.mainClass="edu.buffalo.cse.cse605.BenchmarkRunner" -Dexec.args="8 10 20 10000" > 8-thread.out &
-mvn exec:java -Dexec.mainClass="edu.buffalo.cse.cse605.BenchmarkRunner" -Dexec.args="16 10 20 10000" > 16-thread.out
+iterations=$1
+secondsInRun=$2
+initialListSize=$3
+
+mvn exec:java -Dexec.mainClass="edu.buffalo.cse.cse605.BenchmarkRunner" -Dexec.args="1 $1 $2 $3" > 1-thread.out &
+mvn exec:java -Dexec.mainClass="edu.buffalo.cse.cse605.BenchmarkRunner" -Dexec.args="2 $1 $2 $3" > 2-thread.out &
+mvn exec:java -Dexec.mainClass="edu.buffalo.cse.cse605.BenchmarkRunner" -Dexec.args="4 $1 $2 $3" > 4-thread.out &
+mvn exec:java -Dexec.mainClass="edu.buffalo.cse.cse605.BenchmarkRunner" -Dexec.args="8 $1 $2 $3" > 8-thread.out &
+mvn exec:java -Dexec.mainClass="edu.buffalo.cse.cse605.BenchmarkRunner" -Dexec.args="16 $1 $2 $3" > 16-thread.out
 sleep 120
-mvn exec:java -Dexec.mainClass="edu.buffalo.cse.cse605.BenchmarkRunner" -Dexec.args="32 10 20 10000" > 32-thread.out
+mvn exec:java -Dexec.mainClass="edu.buffalo.cse.cse605.BenchmarkRunner" -Dexec.args="32 $1 $2 $3" > 32-thread.out
 sleep 120
-mvn exec:java -Dexec.mainClass="edu.buffalo.cse.cse605.BenchmarkRunner" -Dexec.args="64 10 20 10000" > 64-thread.out
+mvn exec:java -Dexec.mainClass="edu.buffalo.cse.cse605.BenchmarkRunner" -Dexec.args="64 $1 $2 $3" > 64-thread.out
 
